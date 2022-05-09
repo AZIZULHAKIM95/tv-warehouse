ca677e38626a96e47fe60cc4766f61a54ef2d87b
AZIZULHAKUM95 <kanon.shaharear@gmail.com>
2022-05-09 19:39:00 +0600

    footer section

--- src/Pages/Shared/Foo/Foot.js
+++ src/Pages/Shared/Foo/Foot.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Foot = () => <footer className="page-footer bg-primary font-small blue pt-4">
+const Foot = () => <footer className="page-footer font-small blue pt-4">
     <div className="container-fluid text-center text-md-left">
         <div className="row">
-            <div className="col-md-6 mt-md-0 mt-3">
+            <div className="col-md-6 mt-md-0 mt-3 mx-auto">
                 <h5 className="text-uppercase">Footer Content</h5>
                 <p>Here you can use rows and columns to organize your footer content.</p>
             </div>
@@ -11,29 +11,16 @@ const Foot = () => <footer className="page-footer bg-primary font-small blue pt-
             <hr className="clearfix w-100 d-md-none pb-0"/>
 
             <div className="col-md-3 mb-md-0 mb-3">
-                <h5 className="text-uppercase">Links</h5>
-                <ul className="list-unstyled">
-                    <li><a href="#!">Link 1</a></li>
-                    <li><a href="#!">Link 2</a></li>
-                    <li><a href="#!">Link 3</a></li>
-                    <li><a href="#!">Link 4</a></li>
-                </ul>
+               
             </div>
 
             <div className="col-md-3 mb-md-0 mb-3">
-                <h5 className="text-uppercase">Links</h5>
-                <ul className="list-unstyled">
-                    <li><a href="#!">Link 1</a></li>
-                    <li><a href="#!">Link 2</a></li>
-                    <li><a href="#!">Link 3</a></li>
-                    <li><a href="#!">Link 4</a></li>
-                </ul>
             </div>
         </div>
     </div>
 
     <div className="footer-copyright text-center py-3">© 2020 Copyright:
-        <a href="https://mdbootstrap.com/"> MDBootstrap.com</a>
+      
     </div>
 
 </footer>
